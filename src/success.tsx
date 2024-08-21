e6e147edcfeb6cd33399828154d56a7ad6051536
ushed <137984028+ushed@users.noreply.github.com>
2024-08-21 11:26:39 +0900

    blog_creation

---
+++ src/success.tsx
@@ -0,0 +1,23 @@
+const Success = () => {
+  return (
+    <>
+      <main id="main">
+        <section id="success" class="wrapper">
+          <div class="layout">
+            <div class="success">
+              <h2>Send Success</h2>
+              <p>
+                貴重なご意見ありがとうございます。
+                <br />
+                あなたの意見は本サイト管理者Ushedに送ることができました。
+              </p>
+            </div>
+            <a href="/">home</a>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Success;
